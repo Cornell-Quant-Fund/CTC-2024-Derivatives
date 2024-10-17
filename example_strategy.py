b398f598624f7d71055552338d85b10f5ef72d92
Tanish Tyagi <38384138+anaconda121@users.noreply.github.com>
2024-10-16 23:09:52 -0400

    updated example strategy to take in options and underlying parameters

--- example_strategy.py
+++ example_strategy.py
@@ -4,33 +4,33 @@ from datetime import datetime
 
 class Strategy:
   
-  def __init__(self, start_date, end_date) -> None:
+  def __init__(self, start_date, end_date, options_data, underlying) -> None:
     self.capital : float = 100_000_000
     self.portfolio_value : float = 0
 
     self.start_date : datetime = start_date
     self.end_date : datetime = end_date
   
-    self.options : pd.DataFrame = pd.read_csv("data/cleaned_options_data.csv")
+    self.options : pd.DataFrame = pd.read_csv(options_data)
     self.options["day"] = self.options["ts_recv"].apply(lambda x: x.split("T")[0])
 
-    self.underlying = pd.read_csv("data/underlying_data_hour.csv")
+    self.underlying = pd.read_csv(underlying)
     self.underlying.columns = self.underlying.columns.str.lower()
 
   def generate_orders(self) -> pd.DataFrame:
     orders = []
-    num_orders = 150
+    num_orders = 200
     
     for _ in range(num_orders):
       row = self.options.sample(n=1).iloc[0]
       action = random.choice(["B", "S"])
       
       if action == "B":
-        order_size = random.randint(1, int(row["ask_sz_00"]))
+        if int(row["ask_sz_00"]) > 1:
+          order_size = random.randint(1, int(row["ask_sz_00"]))
       else:
-        order_size = random.randint(1, int(row["bid_sz_00"]))
-
-      assert order_size <= int(row["ask_sz_00"]) or order_size <= int(row["bid_sz_00"])
+        if int(row["bid_sz_00"]) > 1:
+          order_size = random.randint(1, int(row["bid_sz_00"]))
       
       order = {
         "datetime" : row["ts_recv"],
